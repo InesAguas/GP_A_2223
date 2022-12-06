1f18ef1200342720babf1180ddec8a23b3a624b4
InesAguas <inocas_99@hotmail.com>
2022-12-06 21:27:59 +0000

    Models e views
    Acrescentei os models e controllers para produtos e encomendas. Melhorei tambem a página de login para corresponder aos prototipos

--- resources/views/autenticacao.blade.php
+++ resources/views/autenticacao.blade.php
@@ -1,15 +1,30 @@
-<!DOCTYPE html>
-<html>
-    <head>
-    </head>
-    <body>
+@extends('master')
+
+@section('content')
+<div class="container2">
+    <div class="container3">
+        <h2>Iniciar Sessão</h2>
         <form method="POST" action="/login">
-        @csrf
-                <label for="email">Email</label><br>
-                <input type='email' name='email' placeholder='Email'><br>
-                <label for="password">Password</label><br>
-                <input type='password' name='password' placeholder='Password'><br>
-                <input type='submit' class='button' name='submit_login' value='Login'><br>
+            @csrf
+            <label for="email">Email</label><br>
+            <input type='email' name='email' placeholder='Email'><br>
+            <label for="password">Password</label><br>
+            <input type='password' name='password' placeholder='Password'><br>
+            <input type='submit' class='button' name='submit_login' value='Login'><br>
         </form>
-    </body>
-</html>
+    </div>
+    <div class="container3">
+        <h2>Ainda não tens conta?</h2>
+        <ul>
+            <li>Faz encomendas</li>
+            <li>Segue os teus artigos favoritos</li>
+            <li>Tira as tuas dúvidas no nosso chat</li>
+            <p>.... e muito mais</p>
+        </ul>
+        <h3>Não percas mais tempo!</h3>
+        <a href="{{ url('/')}}" ><button type="button">Regista-te</button></a>
+    </div>
+</div>
+@endsection
+
+        
