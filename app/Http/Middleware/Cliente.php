ec2040a50d2394b453ec3f04f25091ff94d01968
InesAguas <inocas_99@hotmail.com>
2022-12-08 16:30:30 +0000

    Middleware
    Foi adicionado ao middleware as verificações de socios e clientes

---
+++ app/Http/Middleware/Cliente.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class Cliente
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(Auth::user()->u_tipo == 3) {
+            return $next($request);
+        }
+        return redirect('/login');
+    }
+}
