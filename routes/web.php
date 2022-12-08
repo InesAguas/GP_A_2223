e4bb41735fd4a5fd9e13197afca13c32c3ca8a8e
InesAguas <inocas_99@hotmail.com>
2022-12-08 16:28:49 +0000

    Routes group
    Foram criados grupos para as routes para facilitar o uso do middleware

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,29 @@ use App\Http\Controllers\ProdutoController;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
 
+//route para aceder à pagina de login
 Route::get('/login', function() {
     return view('autenticacao');
 });
 
+//routes de login e logout
 Route::post('/login', [UserController::class, 'login']);
-
-Route::get('/produtos', [ProdutoController::class, 'verProdutos'])->middleware(['auth','admin']);
-
 Route::get('/logout', [UserController::class, 'logout'])->middleware(['auth']);
 
+//grupo de routes que precisam de verificar se existe alguem autenticado e se é administrador
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/produtos', [ProdutoController::class, 'verProdutos']);
+});
+
+//grupo de routes que precisam de verificar se existe alguem autenticado e se é socio
+Route::middleware(['auth', 'socio'])->group(function () {
+});
+
+//grupo de routes que precisam de verificar se existe alguem autenticado e se é cliente
+Route::middleware(['auth', 'cliente'])->group(function () {
+});
