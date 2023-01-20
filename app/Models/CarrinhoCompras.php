ee44bb3468142522c0bd9e3849acc26ca1484466
Tânia Santos <taniamarsan@gmail.com>
2023-01-20 21:01:01 +0000

    Lista de Desejos

--- app/Models/CarrinhoCompras.php
+++ app/Models/CarrinhoCompras.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Desejo extends Model
+class CarrinhoCompras extends Model
 {
     use HasFactory;
 
